6e46861c9fc9c3de64c40f1305521f7ed6a8b770
hyprd <luuzeish@gmail.com>
2022-09-27 17:20:07 +1300

    SDL/ImGui renderers/windows created

---
+++ definitions.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <cstdio>
+
+int GB_HEIGHT = 160;
+int GB_WIDTH = 144;
--- main.cpp
+++ main.cpp
@@ -1,10 +1,47 @@
 #include "lib/imgui/imgui.h"
 #include "lib/imgui/imgui_impl_sdl.h"
 #include "lib/imgui/imgui_impl_sdlrenderer.h"
+#include "definitions.h"
+
 #include <SDL.h>
 
 int main(int argc, char* argv[])
 {
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("Failed to initialize: %s\n", SDL_GetError());
+        return -1;
+    }
+    SDL_Window* window = SDL_CreateWindow("gbemu", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, GB_HEIGHT * 4, GB_WIDTH * 4, NULL);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL) {
+        SDL_Log("Failed to create SDL renderer");
+        return -1;
+    }
 
-    
+    // Initialize ImGui context
+    ImGui::CreateContext();
+    ImGui::StyleColorsDark();
+    ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
+    ImGui_ImplSDLRenderer_Init(renderer);
+
+    // Main event loop
+    bool end = false;
+    while (!end) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            ImGui_ImplSDL2_ProcessEvent(&event);
+            if (event.type == SDL_QUIT)
+                end = true;
+        }
+    }
+
+    // Clean and close
+    ImGui_ImplSDLRenderer_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
