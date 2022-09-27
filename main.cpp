47a536494afef2edbedddebe68a0560b15eb9d4c
hyprd <luuzeish@gmail.com>
2022-09-27 16:41:02 +1300

    Library preparation, add SDL/ ImGui imports

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "lib/imgui/imgui.h"
+#include "lib/imgui/imgui_impl_sdl.h"
+#include "lib/imgui/imgui_impl_sdlrenderer.h"
+#include <SDL.h>
+
+int main(int argc, char* argv[])
+{
+
+    
+}
